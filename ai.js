4ca7ee1318319974293659a7cf34af42b393c73e
Siddharth Shyniben <siddhart.muscat@gmail.com>
2024-04-29 11:33:27 +0530

    Please forgive me

--- ai.js
+++ ai.js
@@ -3,23 +3,23 @@ const RandomPlayerAI = require("./random-player-ai.js");
 
 const streams = getPlayerStreams(new BattleStream());
 
-const spec = {
-  formatid: "gen7customgame",
-};
+const spec = { formatid: "gen7customgame" };
+
+const team = `Lopunny||Lopunnite|Limber|icepunch,highjumpkick,return,fakeout||85,85,85,85,85,85||||80|]Shedinja||FocusSash|WonderGuard|xscissor,shadowsneak,willowisp,swordsdance||85,85,85,85,85,85|N|||88|]Starmie||ChoiceSpecs|Analytic|scald,psyshock,icebeam,thunderbolt||85,,85,85,85,85|N|,0,,,,||83|]Qwilfish||BlackSludge|Intimidate|thunderwave,toxicspikes,spikes,liquidation||85,85,85,85,85,85||||88|]Bronzong||Leftovers|Levitate|toxic,explosion,earthquake,ironhead||85,85,85,85,85,85|N|||84|]Arcanine||Leftovers|FlashFire|morningsun,toxic,closecombat,flareblitz||77,85,85,85,85,85||||84|`;
+
 const p1spec = {
   name: "Bot 1",
-  team: Teams.pack(Teams.generate("gen7randombattle")),
+  // team: Teams.pack(Teams.generate("gen7randombattle")),
+  team,
 };
 const p2spec = {
   name: "Bot 2",
-  team: Teams.pack(Teams.generate("gen7randombattle")),
+  // team: Teams.pack(Teams.generate("gen7randombattle")),
+  team,
 };
 
 const p1 = new RandomPlayerAI(streams.p1);
 const p2 = new RandomPlayerAI(streams.p2);
-
-console.log("p1 is " + p1.constructor.name);
-console.log("p2 is " + p2.constructor.name);
 
 void p1.start();
 void p2.start();
